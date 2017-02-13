485880508bc83eba00d86a99612cdd8c9af87476
Peti <antalpeti@users.noreply.github.com>
2017-02-13 14:50:45 +0100

    Process ORDER BY examples. #7

---
+++ order_by/order_by.sql
@@ -0,0 +1,7 @@
+SELECT * FROM Customers ORDER BY Country;
+
+SELECT * FROM Customers ORDER BY Country DESC;
+
+SELECT * FROM Customers ORDER BY Country, CustomerName; 
+
+SELECT * FROM Customers ORDER BY Country ASC, CustomerName DESC;
