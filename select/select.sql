591ca67cc0076a998d09e70ed8e98bd06ff24224
Peti <antalpeti@users.noreply.github.com>
2017-02-13 13:36:42 +0100

    Process SELECT examples. #3

--- select/select.sql
+++ select/select.sql
@@ -1,3 +1,7 @@
 SELECT CustomerName,City FROM Customers;
+
+SELECT CustomerName,City,Country FROM Customers;
  
+SELECT CustomerID,CustomerName,City,Country FROM Customers;
+
 SELECT * FROM Customers;
