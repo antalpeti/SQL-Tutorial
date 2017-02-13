a02549c541243e4ee31cb46749d70cb9c0449391
Peti <antalpeti@users.noreply.github.com>
2017-02-13 17:01:06 +0100

    Process SELECT TOP examples. #12

---
+++ select_top/select_top.sql
@@ -0,0 +1,11 @@
+--SQL Server / MS Access Syntax
+SELECT TOP 2 * FROM Customers;
+
+--SQL Server / MS Access Syntax
+SELECT TOP 50 PERCENT * FROM Customers;
+
+--MySQL Syntax
+SELECT * FROM Customers LIMIT 5;
+
+--Oracle Syntax
+SELECT * FROM Customers WHERE ROWNUM <=5;
