d347dd4245cbc78394db12dac6c1739f098dc009
Peti <antalpeti@users.noreply.github.com>
2017-02-13 15:08:13 +0100

    Process INSERT INTO examples. #8

---
+++ insert_into/insert_into.sql
@@ -0,0 +1,7 @@
+INSERT INTO Customers (CustomerName, ContactName, Address, City, PostalCode, Country)
+VALUES ('Cardinal','Tom B. Erichsen','Skagen 21','Stavanger','4006','Norway'); 
+
+INSERT INTO Customers (CustomerName, ContactName, Address, City, PostalCode, Country)
+VALUES ('Tünde','Kis Anna','Füredi utca 2.','Budapest','1144','Hungary'); 
+
+INSERT INTO Customers (CustomerName, City, Country) VALUES ('Cardinal', 'Stavanger', 'Norway'); 
