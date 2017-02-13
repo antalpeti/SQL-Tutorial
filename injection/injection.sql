06ae301affe7ef06d7abcb8ed674c34a73fbf720
Peti <antalpeti@users.noreply.github.com>
2017-02-13 16:50:40 +0100

    Process Injection examples. #11

---
+++ injection/injection.sql
@@ -0,0 +1,5 @@
+SELECT * FROM Customers WHERE CustomerID = 105 or 1=1;
+ 
+SELECT * FROM Customers WHERE CustomerName ="" or ""="" AND ContactName ="" or ""="";
+
+SELECT * FROM Customers WHERE CustomerName = 105; DROP TABLE Suppliers
