32a26985915dcefbd9f8624458322d98d0a2e8e8
Peti <antalpeti@users.noreply.github.com>
2017-02-13 14:23:39 +0100

    Process WHERE examples. #5

---
+++ where/where.sql
@@ -0,0 +1,7 @@
+SELECT * FROM Customers WHERE Country='Mexico';
+
+SELECT * FROM Customers WHERE Country='Spain' AND City = 'Madrid';
+
+SELECT * FROM Customers WHERE CustomerID=1;
+
+SELECT * FROM Customers WHERE CustomerID<=3;
