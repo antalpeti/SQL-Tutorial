90b531980f23db0720c4466f5ecb0d1310c127ce
Peti <antalpeti@users.noreply.github.com>
2017-02-15 15:11:17 +0100

    Process LIKE examples. #13

--- like/like.sql
+++ like/like.sql
@@ -1,17 +1,5 @@
-SELECT * FROM Customers WHERE City LIKE 'ber%'; 
+SELECT * FROM Customers WHERE City LIKE '%s';
 
-SELECT * FROM Customers WHERE PostalCode LIKE '12%'; 
+SELECT * FROM Customers WHERE Country LIKE '%land%';
 
-SELECT * FROM Customers WHERE City LIKE '%es%'; 
-
-SELECT * FROM Customers WHERE City LIKE '_erlin'; 
-
-SELECT * FROM Customers WHERE City LIKE 'L_n_on'; 
-
-SELECT * FROM Customers WHERE City LIKE '[bsp]%';
-
-SELECT * FROM Customers WHERE City LIKE '[a-c]%'; 
-
-SELECT * FROM Customers WHERE City LIKE '[!bsp]%';
-
-SELECT * FROM Customers WHERE City NOT LIKE '[bsp]%';
+SELECT * FROM Customers WHERE Country NOT LIKE '%land%'; 
