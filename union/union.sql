afc4ca91b4e99ee1075369f4bdd7f310ec3820f7
Peti <antalpeti@users.noreply.github.com>
2017-02-21 11:00:33 +0100

    Process UNION examples. #23

---
+++ union/union.sql
@@ -0,0 +1,23 @@
+SELECT City FROM Customers
+UNION
+SELECT City FROM Suppliers
+ORDER BY City; 
+
+SELECT City FROM Customers
+UNION ALL
+SELECT City FROM Suppliers
+ORDER BY City; 
+
+SELECT City, Country FROM Customers
+WHERE Country='Germany'
+UNION 
+SELECT City, Country FROM Suppliers
+WHERE Country='Germany'
+ORDER BY City; 
+
+SELECT City, Country FROM Customers
+WHERE Country='Germany'
+UNION ALL
+SELECT City, Country FROM Suppliers
+WHERE Country='Germany'
+ORDER BY City; 
