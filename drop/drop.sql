0fa306e3dd7b3d918e63dfa68811f648efcf6743
Peti <antalpeti@users.noreply.github.com>
2017-03-07 16:06:29 +0100

    Process DROP examples. #36

---
+++ drop/drop.sql
@@ -0,0 +1,17 @@
+--DROP INDEX Syntax for MS Access
+DROP INDEX index_name ON table_name;
+
+--DROP INDEX Syntax for MS SQL Server
+DROP INDEX table_name.index_name;
+
+--DROP INDEX Syntax for DB2/Oracle
+DROP INDEX index_name;
+
+--DROP INDEX Syntax for MySQL
+ALTER TABLE table_name DROP INDEX index_name;
+
+DROP TABLE table_name;
+
+DROP DATABASE database_name;
+
+TRUNCATE TABLE table_name;
